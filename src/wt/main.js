c94084f107d5cec2639e29afc4e46ef0696692a8
Sleeping0w1 <1033351@gmail.com>
2025-10-27 00:34:37 +0300

    feat: add solution for wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,29 @@
+import { Worker } from "worker_threads";
+import os from "os";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const workerFile = path.join(__dirname, "worker.js");
+
 const performCalculations = async () => {
-  // Write your code here
+  const promises = os.cpus().map((cup, index) => {
+    return new Promise((resolve) => {
+      const worker = new Worker(workerFile, { workerData: 10 + index });
+      worker.on("message", (data) => {
+        resolve({ status: "resolved", data: data });
+      });
+      worker.on("error", () => {
+        resolve({
+          status: "error",
+          data: null,
+        });
+      });
+    });
+  });
+  console.log('Results:', await Promise.all(promises));
 };
 
 await performCalculations();
