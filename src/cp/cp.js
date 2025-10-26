5471d4386998f6f15f8980495ae4446554674001
Sleeping0w1 <1033351@gmail.com>
2025-10-27 00:57:52 +0300

    feat: add solution for cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,15 @@
+import { spawn } from "child_process";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const scriptFile = path.join(__dirname, "files/script.js");
+
 const spawnChildProcess = async (args) => {
-  // Write your code here
+  spawn('node', [scriptFile, ...(Array.isArray(args) ? args : [args])], {stdio: 'inherit'});
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([123, 11, 3]);
